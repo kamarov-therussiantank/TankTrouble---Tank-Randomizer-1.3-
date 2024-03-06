51d833b365032c0fe78e87c810d99f09bbe93535
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2024-03-06 15:13:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const Add_Custom_Style = css => document.head.appendChild(document.createElement
 // Custom Element
 
 function Create_Custom_Element(tag, attr_tag, attr_name, value) {
-  const custom_element = document.customElement(tag)
+  const custom_element = document.createElement(tag)
   custom_element.setAttribute(attr_tag, attr_name)
   custom_element.innerHTML = (value)
   document.body.append(custom_element)
