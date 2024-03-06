15c65ccd376c41346e328aaba2bfa152e2eeb2af
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2024-03-06 14:14:42 +0100

    Delete contentScript.js

--- contentScript.js
+++
@@ -1,6 +0,0 @@
-const linkElement = document.createElement('link');
-linkElement.rel = 'stylesheet';
-linkElement.type = 'text/css';
-linkElement.href = chrome.runtime.getURL('style.css');
-
-document.head.appendChild(linkElement);
