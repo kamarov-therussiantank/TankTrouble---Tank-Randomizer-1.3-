5cc5c4323eb91b724a434868e3d4932e85c9cd72
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2024-03-06 15:30:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-// Get URL
+// Get the hostname of the current website
+const site = window.location.hostname;
 
-const site = window.location.hostname
+// Function to dynamically add custom CSS
+const addCustomStyle = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
 
-// Custom CSS
-
-const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css
-
-// Custom Element
-
-function Create_Custom_Element(tag, attr_tag, attr_name, value) {
-  const custom_element = document.createElement(tag)
-  custom_element.setAttribute(attr_tag, attr_name)
-  custom_element.innerHTML = (value)
-  document.body.append(custom_element)
+// Function to create a custom HTML element
+function createCustomElement(tag, attr_tag, attr_name, value) {
+  const custom_element = document.createElement(tag);
+  custom_element.setAttribute(attr_tag, attr_name);
+  custom_element.innerHTML = value;
+  document.body.append(custom_element);
 }
 
-//JS codes for TT
+// Create a link element for linking the style.css file
+const linkElement = document.createElement('link');
+linkElement.rel = 'stylesheet';
+linkElement.href = chrome.runtime.getURL('style.css');
+document.head.appendChild(linkElement);
+
+// JavaScript codes for TankTrouble
 if (site.includes("tanktrouble.com")) {
-  Add_Custom_Style(`
+  addCustomStyle(`
 .snippet {
-    background: linear-gradient(to bottom, #c2c2c2, #c2c2c2);
+    background: linear-gradient(to bottom, #d9d9d9, #d9d9d9);
     border: #cfcfcf 2px solid;
 }
 
 .forum .bubble {
-    background-color: #fff;
+    background-color: #f2f2f2;
     border: #333 2px solid;
-    font-family: 'TankTroubleClassic';
-    
+    font-family: 'TankTroubleClassic'; // Fix the typo here
 }
-  `)
+  `);
 }
