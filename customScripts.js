a778b605f126f1722db4b61f41817c619359590f
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2024-03-31 23:40:47 +0200

    Update customScripts.js

--- customScripts.js
+++ customScripts.js
@@ -1,4 +1,5 @@
 if (window.location.hostname.includes("tanktrouble.com")) {
+    // Injection
     function injectJSCode(code) {
         const scriptElement = document.createElement('script');
         scriptElement.setAttribute('type', 'text/javascript');
@@ -151,4 +152,27 @@ if (window.location.hostname.includes("tanktrouble.com")) {
             }
         }
     });
+
+    // Up to 6 Games script
+    UIConstants.GAME_ICON_POOL_SIZE = 6;
+    UIConstants.GAME_ICON_COUNT = 6;
+    UIConstants.GAME_ICON_WIDTH = UIConstants.GAME_ICON_WIDTH / 1.9;
+    UIConstants.GAME_ICON_HEIGHT = UIConstants.GAME_ICON_HEIGHT / 1.9;
+    UIGameIconImage.prototype.spawn = function(x, y, gameState, favouriteActiveQueuedCounts) {
+        this.reset(x, y);
+        this.gameId = gameState.getId();
+        this.mode = gameState.getMode();
+        this.ranked = gameState.getRanked();
+        this.playerStates = gameState.getPlayerStates();
+        this.favouriteActiveQueuedCounts = favouriteActiveQueuedCounts;
+        this._updateUI();
+        var delay = 50 + Math.random() * 200;
+        if (this.removeTween) {
+            this.removeTween.stop();
+        }
+        this.game.add.tween(this.scale).to({
+            x: UIConstants.ASSET_SCALE / 1.9,
+            y: UIConstants.ASSET_SCALE / 1.9
+        }, UIConstants.ELEMENT_POP_IN_TIME, Phaser.Easing.Back.Out, true, delay);
+    };
 }
