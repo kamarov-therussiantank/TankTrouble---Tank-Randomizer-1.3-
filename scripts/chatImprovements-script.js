1233060224881880142a984edac2b2aab3267789
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2024-04-13 14:29:45 +0200

    Update chatImprovements-script.js

--- scripts/chatImprovements-script.js
+++ scripts/chatImprovements-script.js
@@ -14,13 +14,13 @@ if (window.location.hostname.includes("tanktrouble.com")) {
         document.documentElement.appendChild(scriptElement);
     }
 
-  // Function to dynamically add custom CSS
-const addCustomStyle = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
+    // Function to dynamically add custom CSS
+    const addCustomStyle = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
 
-  // Get the hostname of the current website
-const site = window.location.hostname;
-if (site.includes("tanktrouble.com")) {
-   addCustomStyle(`
+    // Get the hostname of the current website
+    const site = window.location.hostname;
+    if (site.includes("tanktrouble.com")) {
+        addCustomStyle(`
 #chat {
   /*Move it to the bottom left*/
   inset: calc(100% - 30px) auto auto 34px !important;
@@ -88,7 +88,7 @@ if (site.includes("tanktrouble.com")) {
 body:has(#chat .body.ui-resizable-resizing) .ui-resizable-handle.handle.ui-resizable-ne {
   display: none !important;
 }
- 
+
 /* Scrollbar */
 #chat .body {
   scrollbar-gutter: stable;
@@ -117,133 +117,134 @@ body:has(#chat .body.ui-resizable-resizing) .ui-resizable-handle.handle.ui-resiz
   background: rgb(170, 170, 170);
 }
 `);
- 
-/**
- * Reconfigure the chat handle to be dragging
- * from the south-east direction (down)
- * to the north-east direction (up)
- */
-const changeHandleDirection = () => {
-	const { resizable } = $.fn;
- 
-	// Use a regular function to keep context
-	$.fn.resizable = function(...args) {
-		const [config] = args;
- 
-		// Reassign the chat handle to be north-east facing
-		if (config.handles) {
-			const handle = config.handles.se;
-			if (handle === TankTrouble.ChatBox.chatBodyResizeHandle) {
-				handle.removeClass('ui-resizable-se')
-					.addClass('ui-resizable-ne');
- 
-				config.handles.ne = handle;
-				delete config.handles.se;
- 
-				// Set a taller chat maxHeight
-				config.maxHeight = 650;
-			}
-		}
- 
-		return resizable.call(this, config);
-	};
-};
- 
-/**
- * Hook message render functions to disable jquery .show() animation
- * This fixes chat messages not showing up in the reversed chat order
- */
-const fixChatRendering = () => {
-	Loader.hookFunction(TankTrouble.ChatBox, '_renderChatMessage', (original, ...args) => {
-		// Set animateHeight to false
-		args[9] = false;
-		original(...args);
-	});
- 
-	Loader.hookFunction(TankTrouble.ChatBox, '_renderSystemMessage', (original, ...args) => {
-		// Set animateHeight to false
-		args[3] = false;
-		original(...args);
-	});
-};
- 
-/**
- * Prevent TankTrouble from clearing the chat when the client disconnects
- * Print message to chat when client switches server to separate conversations
- */
-const preventChatClear = () => {
-	Loader.hookFunction(TankTrouble.ChatBox, '_clearChat', (original, ...args) => {
-		const isUnconnected = ClientManager.getClient().getState() === TTClient.STATES.UNCONNECTED;
- 
-		// Void the call if the client is unconnected
-		// when the function is invoked
-		if (isUnconnected) return null;
- 
-		return original(...args);
-	});
- 
-	Loader.hookFunction(TankTrouble.ChatBox, '_updateStatusMessageAndAvailability', (original, ...args) => {
-		const [systemMessageText, guestPlayerIds] = args;
- 
-		// Check for welcome message
-		// If true, print a system message
-		if (systemMessageText === 'Welcome to TankTrouble Comms § § ') {
-			const newServer = ClientManager.getAvailableServers()[ClientManager.multiplayerServerId];
-			return original(`Connected to ${ newServer.name } ${ guestPlayerIds.length ? '§ ' : '' }`, guestPlayerIds);
-		}
-		return original(...args);
-	});
-};
- 
-/**
- * Write the chat savestate to storage and return
- * @returns Promise for last savestate
- */
-const startChatSavestate = () => {
-	Loader.hookFunction(TankTrouble.ChatBox, 'open', (original, ...args) => {
-		GM_setValue('chat-open', true);
-		original(...args);
-	});
-	Loader.hookFunction(TankTrouble.ChatBox, 'close', (original, ...args) => {
-		GM_setValue('chat-open', false);
-		original(...args);
-	});
- 
-	// Get savestate and default to chat being open
-	return GM_getValue('chat-open', true);
-};
- 
-changeHandleDirection();
-fixChatRendering();
-Loader.whenContentInitialized().then(async() => {
-	preventChatClear();
- 
-	const shouldChatOpen = await startChatSavestate();
-	if (shouldChatOpen) TankTrouble.ChatBox.open();
- 
-	// Get the plain nodes for better performance
-	// eslint-disable-next-line prefer-destructuring
-	const chatBody = TankTrouble.ChatBox.chatBody[0];
-	// eslint-disable-next-line prefer-destructuring
-	const chatForm = TankTrouble.ChatBox.chatForm[0];
-	// eslint-disable-next-line prefer-destructuring
-	const chatInput = TankTrouble.ChatBox.chatInput[0];
- 
-	// Create a mutation observer that looks for changes in the chatBody's attributes (namely width)
-	new MutationObserver(() => {
-		const width = Number(chatBody.offsetWidth || 220);
- 
-		chatForm.style.width = `${width}px`;
-		chatInput.style.width = `${width - 12}px`;
-	}).observe(chatBody, {
-		attributes: true,
-		characterData: false
-	});
- 
-	chatForm.style.width = '220px';
-	chatInput.style.width = `${chatForm.offsetWidth - 12}px`;
- 
-	// Allow more characters in the chat input
-	chatInput.setAttribute('maxlength', '255');
-});
+
+        /**
+         * Reconfigure the chat handle to be dragging
+         * from the south-east direction (down)
+         * to the north-east direction (up)
+         */
+        const changeHandleDirection = () => {
+            const { resizable } = $.fn;
+
+            // Use a regular function to keep context
+            $.fn.resizable = function(...args) {
+                const [config] = args;
+
+                // Reassign the chat handle to be north-east facing
+                if (config.handles) {
+                    const handle = config.handles.se;
+                    if (handle === TankTrouble.ChatBox.chatBodyResizeHandle) {
+                        handle.removeClass('ui-resizable-se')
+                            .addClass('ui-resizable-ne');
+
+                        config.handles.ne = handle;
+                        delete config.handles.se;
+
+                        // Set a taller chat maxHeight
+                        config.maxHeight = 650;
+                    }
+                }
+
+                return resizable.call(this, config);
+            };
+        };
+
+        /**
+         * Hook message render functions to disable jquery .show() animation
+         * This fixes chat messages not showing up in the reversed chat order
+         */
+        const fixChatRendering = () => {
+            Loader.hookFunction(TankTrouble.ChatBox, '_renderChatMessage', (original, ...args) => {
+                // Set animateHeight to false
+                args[9] = false;
+                original(...args);
+            });
+
+            Loader.hookFunction(TankTrouble.ChatBox, '_renderSystemMessage', (original, ...args) => {
+                // Set animateHeight to false
+                args[3] = false;
+                original(...args);
+            });
+        };
+
+        /**
+         * Prevent TankTrouble from clearing the chat when the client disconnects
+         * Print message to chat when client switches server to separate conversations
+         */
+        const preventChatClear = () => {
+            Loader.hookFunction(TankTrouble.ChatBox, '_clearChat', (original, ...args) => {
+                const isUnconnected = ClientManager.getClient().getState() === TTClient.STATES.UNCONNECTED;
+
+                // Void the call if the client is unconnected
+                // when the function is invoked
+                if (isUnconnected) return null;
+
+                return original(...args);
+            });
+
+            Loader.hookFunction(TankTrouble.ChatBox, '_updateStatusMessageAndAvailability', (original, ...args) => {
+                const [systemMessageText, guestPlayerIds] = args;
+
+                // Check for welcome message
+                // If true, print a system message
+                if (systemMessageText === 'Welcome to TankTrouble Comms § § ') {
+                    const newServer = ClientManager.getAvailableServers()[ClientManager.multiplayerServerId];
+                    return original(`Connected to ${ newServer.name } ${ guestPlayerIds.length ? '§ ' : '' }`, guestPlayerIds);
+                }
+                return original(...args);
+            });
+        };
+
+        /**
+         * Write the chat savestate to storage and return
+         * @returns Promise for last savestate
+         */
+        const startChatSavestate = () => {
+            Loader.hookFunction(TankTrouble.ChatBox, 'open', (original, ...args) => {
+                GM_setValue('chat-open', true);
+                original(...args);
+            });
+            Loader.hookFunction(TankTrouble.ChatBox, 'close', (original, ...args) => {
+                GM_setValue('chat-open', false);
+                original(...args);
+            });
+
+            // Get savestate and default to chat being open
+            return GM_getValue('chat-open', true);
+        };
+
+        changeHandleDirection();
+        fixChatRendering();
+        Loader.whenContentInitialized().then(async() => {
+            preventChatClear();
+
+            const shouldChatOpen = await startChatSavestate();
+            if (shouldChatOpen) TankTrouble.ChatBox.open();
+
+            // Get the plain nodes for better performance
+            // eslint-disable-next-line prefer-destructuring
+            const chatBody = TankTrouble.ChatBox.chatBody[0];
+            // eslint-disable-next-line prefer-destructuring
+            const chatForm = TankTrouble.ChatBox.chatForm[0];
+            // eslint-disable-next-line prefer-destructuring
+            const chatInput = TankTrouble.ChatBox.chatInput[0];
+
+            // Create a mutation observer that looks for changes in the chatBody's attributes (namely width)
+            new MutationObserver(() => {
+                const width = Number(chatBody.offsetWidth || 220);
+
+                chatForm.style.width = `${width}px`;
+                chatInput.style.width = `${width - 12}px`;
+            }).observe(chatBody, {
+                attributes: true,
+                characterData: false
+            });
+
+            chatForm.style.width = '220px';
+            chatInput.style.width = `${chatForm.offsetWidth - 12}px`;
+
+            // Allow more characters in the chat input
+            chatInput.setAttribute('maxlength', '255');
+        });
+    }
 }
