83aec79f473ac3a55a21afb02de8ab7ec233c407
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2024-04-14 14:08:51 +0200

    Update contentScripts.js

--- contentScripts.js
+++ contentScripts.js
@@ -96,6 +96,17 @@ if (site.includes("tanktrouble.com")) {
     border-radius: 0px;
     box-shadow: 0 5px 7px 0 rgba(0,0,0, .5);
    }
+   #overlay .newGame .premium::before {
+    content: "";
+    width: 160px;
+    height: 180px;
+    position: absolute;
+    top: -115px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-image: url(https://cdn.tanktrouble.com/RELEASE-2023-09-06-01/assets/images/tankInfo/accountActive@2x.png);
+    background-size: 160px 180px;
+}
     #overlay .messages .message {
         background: #1ad72a;
         color: #fff;
