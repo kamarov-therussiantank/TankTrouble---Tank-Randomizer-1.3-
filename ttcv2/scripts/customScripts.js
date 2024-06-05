b8b211957a2b79b1b5f9f6b1e33e92e8fc584d37
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2024-06-05 15:47:27 +0200

    Update customScripts.js

--- ttcv2/scripts/customScripts.js
+++ ttcv2/scripts/customScripts.js
@@ -311,47 +311,6 @@ div#shopItem-122 {
 }
        `);
 
-function createCustomElement(tag, attr_tag, attr_name, value) {
-  const custom_element = document.createElement(tag);
-  custom_element.setAttribute(attr_tag, attr_name);
-  custom_element.innerHTML = value;
-  document.getElementById('tertiaryContent').appendChild(custom_element);
-}
-// Get the hostname of the current website
-const site = window.location.hostname;
-// JavaScript codes for TankTrouble
-if (site.includes("tanktrouble.com")) {
-   createCustomElement('div', 'id', 'helpSnippet', `
-   <style>
-    #helpSnippet {
-      background: linear-gradient(to bottom, #000, #000);
-      border: #000 3px solid;
-      border-radius: 5px;
-      box-shadow: none;
-      margin-bottom: 10px;
-      text-align: center;
-    }
-    #helpSnippet .header {
-      font-family:'TankTrouble';
-      font-size: 14px;
-      color: #fff;
-      margin-bottom: 5px;
-      padding: 5px 0 3px 0;
-    }
-    #helpSnippet .content {
-    margin: 3px 1px 3px 1px;
-    font-family: 'ClassicTankTrouble';
-    font-size: 1.05em;
-    }
-    #helpSnippet .content * {
-      margin: 3px 1px 3px 1px;
-    }
-  </style>
-    <div class="content">
-      <div class="header">Need Help?</div>
-      <a href="https://tinyurl.com/ttrulesdoc" target="_blank" style="color: #fff; text-decoration: underline;">Check the FAQ</a>
-  `);
-	    
 //Classic Mouse script
     Tank.method('_computeRotationSpeed', function() {
         this.rotationSpeed = 0;
